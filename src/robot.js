51e781bc20285776883f8be0b15708bf8870f417
Amber <amber.quynh.nguyen@gmail.com>
2025-03-01 18:24:33 +0800

    robot class created

--- src/robot.js
+++ src/robot.js
@@ -1,3 +1,90 @@
 import Table from "./table.js";
-const myTable = new Table();
-console.log(myTable.isValidPosition(1, 1));
+
+class Robot {
+  constructor() {
+    this.x = null;
+    this.y = null;
+    this.facing = null;
+    this.placed = false;
+    this.table = new Table();
+  }
+
+  place(x, y, facing) {
+    if (this.table.isValidPosition(x, y)) {
+      this.x = x;
+      this.y = y;
+      this.facing = facing;
+      this.placed = true;
+    }
+  }
+
+  move() {
+    if (!this.placed) return;
+
+    let newX = this.x;
+    let newY = this.y;
+
+    switch (this.facing) {
+      case "NORTH":
+        newY++;
+        break;
+      case "SOUTH":
+        newY--;
+        break;
+      case "EAST":
+        newX++;
+        break;
+      case "WEST":
+        newX--;
+        break;
+    }
+
+    if (this.table.isValidPosition(newX, newY)) {
+      this.x = newX;
+      this.y = newY;
+    }
+  }
+
+  left() {
+    if (!this.placed) return;
+    switch (this.facing) {
+      case "NORTH":
+        this.facing = "WEST";
+        break;
+      case "SOUTH":
+        this.facing = "EAST";
+        break;
+      case "EAST":
+        this.facing = "NORTH";
+        break;
+      case "WEST":
+        this.facing = "SOUTH";
+        break;
+    }
+  }
+  right() {
+    if (!this.placed) return;
+    switch (this.facing) {
+      case "NORTH":
+        this.facing = "EAST";
+        break;
+      case "SOUTH":
+        this.facing = "WEST";
+        break;
+      case "EAST":
+        this.facing = "SOUTH";
+        break;
+      case "WEST":
+        this.facing = "NORTH";
+        break;
+    }
+  }
+
+  report() {
+    if (this.placed) {
+      console.log(`Output: ${this.x}, ${this.y}, ${this.facing}`);
+    }
+  }
+}
+
+export default Robot;

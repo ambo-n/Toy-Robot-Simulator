1aaf5c6a85a203b4b01d58b33ba262daf82fc0d5
Amber <amber.quynh.nguyen@gmail.com>
2025-03-01 17:17:35 +0800

    table.js created. Add .gitignore file

---
+++ src/table.js
@@ -0,0 +1,12 @@
+class Table {
+  boundary(width = 5, height = 5) {
+    this.width = width;
+    this.height = height;
+  }
+
+  isValidPosition(x, y) {
+    return x >= 0 && x < this.width && y >= 0 && y < this.height;
+  }
+}
+
+module.exports = Table;
